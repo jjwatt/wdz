22247064e1f652b1825c8865467e59a4d228939c
Jesse Wattenbarger <jwattenb@redhat.com>
2024-08-28 13:14:43 -0400

    thrashing on this a little learning zig
    hard way

--- wdz.zig
+++ wdz.zig
@@ -1,21 +1,34 @@
 const std = @import("std");
 
 pub fn main() !void {
+    //const path: ?[]u8 = try getCwd() orelse "";
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    var allocator = gpa.allocator();
 
+    const thing = try getCwd(&allocator);
+    defer allocator.free(thing);
+    std.debug.print("thing is {?s}\n", .{thing});
+}
+
+pub fn getCwd(allocator: *std.mem.Allocator) !?[]u8 {
     const d: std.fs.Dir = std.fs.cwd();
     std.debug.print("cwd is {d}\n", d);
     std.debug.print("trying to look in value: {}\n", .{d});
 
     const path = try d.realpathAlloc(allocator, ".");
-    defer allocator.free(path);
     std.debug.print("Current directory: {s}\n", .{path});
+    return path;
 }
-// add
-// del/rm
-// ls/list
-pub const Bookmark = struct { name: []u8, path: []u8 };
-pub const BookmarkFile = struct { path: []u8 = "~/.wdz" };
+// cwd (call real fs.cwd() or override for testing)
+// add (Bookmark, BookmarkFile)
+// dirAdd (name: []u8, directory: fs.Dir, BookmarkFile)
+// del/rm (Identifier, BookmarkFile)
+// ls/list (Filter, BookmarkFile)
+// readfile (BookmarkFile)
+// writefile (BookmarkFile)
+// Identifier would just be a string that we can check against multiple fields
+// Filter would be a string that we can search or maybe glob or re
+// pub const Bookmark = struct { name: []u8, path: []u8 };
+// pub const BookmarkFile = struct { path: []u8 = "~/.wdz" };
 // BookmarkFile .content?
