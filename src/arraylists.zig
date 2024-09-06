ab2d81a21f40954f3d7927d5fa2cd3e6983cfc66
Jesse Wattenbarger <jwattenb@redhat.com>
2024-09-06 08:37:09 -0400

    learning a lot about arraylists and slices and memory
    changes in arraylists.zig

--- src/arraylists.zig
+++ src/arraylists.zig
@@ -2,34 +2,19 @@ const std = @import("std");
 const mem = std.mem;
 
 test "messing with arraylist" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
-
-    // var lines = std.ArrayList([]const u8).init(allocator);
-    // defer lines.deinit();
-
-    // testing append
-    // try lines.append("zig-out|/home/zig/zig-out");
-    // try lines.append("home|/home/jwattenb");
-
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
     const lines = try buildArrayListReverse(allocator);
     defer lines.deinit();
     for (lines.items) |line| {
         std.debug.print("{s}\n", .{line});
     }
-    try buildArrayListReverseIter(allocator);
-}
-
-pub fn buildArrayList(allocator: mem.Allocator) !std.ArrayList([]const u8) {
-    // build an ArrayList and try to return it
-    var lines = std.ArrayList([]const u8).init(allocator);
-    // defer lines.deinit();
-    try lines.append("zig-out|/home/zig/zig-out");
-    try lines.append("home|/home/jwattenb");
-    try lines.append("zig|/home/zig");
-
-    return lines;
+    const rev = try buildArrayListReverseIter(allocator);
+    std.debug.print("printing after reverseiter...\n", .{});
+    for (rev.items) |item| {
+        std.debug.print("{s}\n", .{item});
+    }
 }
 pub fn buildArrayListReverse(allocator: mem.Allocator) !std.ArrayList([]const u8) {
     const lines = try buildArrayList(allocator);
@@ -40,11 +25,37 @@ pub fn buildArrayListReverse(allocator: mem.Allocator) !std.ArrayList([]const u8
     }
     return backwards;
 }
-pub fn buildArrayListReverseIter(allocator: mem.Allocator) !void {
+pub fn buildArrayListReverseIter(allocator: mem.Allocator) !std.ArrayList([]const u8) {
     var lines = try buildArrayList(allocator);
     // defer lines.deinit();
-    const sl = lines.toOwnedSlice();
+    const sl = try lines.toOwnedSlice();
     std.debug.print("type of sl: {}\n", .{@TypeOf(sl)});
-    std.debug.print("trying to print with reverseIterator...", .{});
+    // std.debug.print("trying to print with reverseIterator...\n", .{});
+    // var it = mem.reverseIterator(sl);
+    // while (it.next()) |s| {
+    //     std.debug.print("{s}\n", .{s});
+    // }
+    // Loop through the list backwards and build a new list.
+    var it = mem.reverseIterator(sl);
+    var backwards = std.ArrayList([]const u8).init(allocator);
+    while (it.next()) |s| {
+        try backwards.append(s);
+    }
+    return backwards;
+    // std.debug.print("printing from backwards items\n", .{});
+    // for (backwards.items) |item| {
+    //     std.debug.print("{s}\n", .{item});
+    // }
 }
+pub fn buildArrayList(allocator: mem.Allocator) !std.ArrayList([]const u8) {
+    // build an ArrayList and try to return it
+    var lines = std.ArrayList([]const u8).init(allocator);
+    // defer lines.deinit();
+    try lines.append("zig-out|/home/zig/zig-out");
+    try lines.append("home|/home/jwattenb");
+    try lines.append("zig|/home/zig");
+
+    return lines;
+}
+
 // pub fn main() !void {}
