db2bf4aa530ab3eb6b321f87beab1c2ca9aaa05b
Jesse Wattenbarger <jwattenb@redhat.com>
2024-09-06 17:18:15 -0400

    still messing with arraylists
    adding timing and some other functions

--- src/arraylists.zig
+++ src/arraylists.zig
@@ -39,20 +39,30 @@ pub fn buildArrayListReverse(allocator: mem.Allocator) !std.ArrayList([]const u8
     return backwards;
 }
 pub fn buildArrayListReverseIter(allocator: mem.Allocator) !std.ArrayList([]const u8) {
+    // This seems more complex than the one that inserts into a new
+    // array list, but it tests 2-3x faster than the other one,
+    // probably mostly due to appending vs. inserting.
     var lines = try buildArrayList(allocator);
     defer lines.deinit();
     const sl = try lines.toOwnedSlice();
-    std.debug.print("type of sl: {}\n", .{@TypeOf(sl)});
+
     // Loop through the list backwards and build a new list.
     // I convert lines to toOwnedSlice() because mem.reverseIterator()
     // seems to work on ArrayList, but then it.next() complains about
     // the type and throws an error from zig's std code.
+    // .toOwnedSlice() will drain "lines" and let the caller own
+    // the memory.
     var it = mem.reverseIterator(sl);
     var backwards = std.ArrayList([]const u8).init(allocator);
     while (it.next()) |s| {
         try backwards.append(s);
     }
     return backwards;
+}
+pub fn reverseListPrint(allocator: mem.Allocator) !void {
+    // this will get the list and print it in reverse to stdout
+    // does not return anything or allocate the extra ArrayList
+    // var lines = try buildArrayList(allocator);
 }
 pub fn buildArrayList(allocator: mem.Allocator) !std.ArrayList([]const u8) {
     // build an ArrayList and try to return it
